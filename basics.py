588fa26c82ba53f3cd0d92132f1c8ce521f7e42f
Parth Bhatia <pbhatia_be23@thapar.edu>
2025-02-11 15:39:33 +0530

    basic of python

---
+++ basics.py
@@ -0,0 +1,6 @@
+print("hello world")
+print(5.6)
+print("india", 5 , True)
+print("india",5,True, sep='-')
+print("hello", end=' ')
+print("world")
---
+++ datatypes.py
@@ -0,0 +1,14 @@
+##python has multiple data type 
+# A: basic types(integer, floats ,etc)
+# B: container types
+# 1. list (same as array)
+print([1,2,3,4,5,6])
+# 2. tuples
+print((1,2,3,4,5))  
+# 3.sets
+print({1,2,3,4,5,6})
+# 4. dictionary
+print({"name":"Nitish","Age":30})
+# C: user defined data types (class)
+# pyhton hs integers with infinite data space 
+# pyhton supports complpex numbers 
