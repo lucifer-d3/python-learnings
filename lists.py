700acb787c6abf18cea857024cb17cc346191a33
Parth Bhatia <pbhatia_be23@thapar.edu>
2025-04-02 23:01:46 +0530

    genderbiasaiproject

---
+++ lists.py
@@ -0,0 +1,17 @@
+# list is a data type where we can store multiple things together 
+# list is fundamnetally same to the array in c 
+# array are homogenous(sabhi subjects ek hi data type ke honge)list can be hetrogenous 
+# array has continous memory location but in list it's not the same 
+# array is much faster than list 
+# list are more programmer friendly
+#TO create an empty list 
+L=[]
+L2=['HELLO',1332, True,33 ]
+L3=list ('Haldia')
+L4=[1,2,3,4,5,[7,9,8,6]]
+#append to add aanything in last of the list 
+#extend can add multiple items to the end 
+#insert  to insert at desired loction in a list
+print(L2)
+
+
